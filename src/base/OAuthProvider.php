7218f2bc0ef6a286c7bd1933b94c485f30b2e6f8
Josh Crawford <joshua@sgroup.com.au>
2024-04-28 10:32:08 +1000

    Add support for `headlessMode` redirect URIs

--- src/base/OAuthProvider.php
+++ src/base/OAuthProvider.php
@@ -50,14 +50,13 @@ abstract class OAuthProvider extends Provider implements OAuthProviderInterface
 
     public function getRedirectUri(): ?string
     {
+        $siteId = Craft::$app->getSites()->getCurrentSite()->id ?? Craft::$app->getSites()->getPrimarySite()->id;
+
         // Check for Headless Mode and use the Action URL, or when `cpTrigger` is empty to signify split front/back-end
         if (Craft::$app->getConfig()->getGeneral()->headlessMode || !Craft::$app->getConfig()->getGeneral()->cpTrigger) {
-            return UrlHelper::actionUrl('social-share/auth/callback');
+            return UrlHelper::cpUrl('social-share/auth/callback', null, null, $siteId);
         }
-        
-        $siteId = Craft::$app->getSites()->getPrimarySite()->id;
 
-        // We should always use the primary site for the redirect
         return UrlHelper::siteUrl('social-share/auth/callback', null, null, $siteId);
     }
 
